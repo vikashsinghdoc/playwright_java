b8b5f2be26a18f065d5d62dd9efaa25fa68d3320
vikashsinghdoc <vikashsinghdoc@gmail.com>
2025-10-06 17:44:43 +0530

    added some common methods for use

--- src/main/java/com/playwright/framework/pages/HomePage.java
+++ src/main/java/com/playwright/framework/pages/HomePage.java
@@ -8,18 +8,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class HomePage extends BasePage {
 
-    @Autowired
-    private PlaywrightDriver playwrightDriver;
     public void mouseOverNewCar(){
+        highlightElement("new_cars");
         mouseHover("new_cars");
     }
     public void clickFindNewCars(){
+        highlightElement("find_new_cars");
         click("find_new_cars");
     }
 
-    public void openHomePage(String url) {
-        this.playwrightDriver.getPage().navigate(url);
+    public void openHomePage(String url){
+        navigateToUrl(url);
     }
-
 
 }
--- src/main/java/com/playwright/framework/pages/NewCarsPage.java
+++ src/main/java/com/playwright/framework/pages/NewCarsPage.java
@@ -7,12 +7,15 @@ import org.springframework.stereotype.Component;
 public class NewCarsPage extends BasePage {
 
     public void goToToyota(){
+        highlightElement("toyota_xpath");
         click("toyota_xpath");
     }
     public void goToHonda(){
+        highlightElement("honda_xpath");
         click("honda_xpath");
     }
     public void goToKia(){
+        highlightElement("kia_xpath");
         click("kia_xpath");
     }
 }
