cf9de9a80f6e8994392e4348f22d095ca28df0da
vikashsinghdoc <vikashsinghdoc@gmail.com>
2025-10-08 16:30:21 +0530

    adding Bannger type logs & guarding verbose logging in case of frame search/highlight to be implemented

--- src/main/java/com/playwright/framework/steps/Hooks.java
+++ src/main/java/com/playwright/framework/steps/Hooks.java
@@ -1,5 +1,6 @@
 package com.playwright.framework.steps;
 
+import com.playwright.framework.log.FrameLoggingConfig;
 import com.playwright.framework.log.Log;
 import com.playwright.framework.driver_manager.PlaywrightDriver;
 import io.cucumber.java.After;
@@ -12,22 +13,31 @@ public class Hooks {
     @Autowired
     private PlaywrightDriver playwrightDriver;
 
-    @Before
+    private static boolean frameFlagLogged = false;
+
+    @Before(order = 0)
+    public void logFrameFlag() {
+        if (!frameFlagLogged) {
+            frameFlagLogged = true;
+            Log.bannerInfo("FrameSearch & Highlight flags=" + FrameLoggingConfig.resolve());
+        }
+    }
+    @Before(order = 1)
     public void setupDriver(){
         this.playwrightDriver.playwrightDriverCreation();
-        Log.info("******************************** Created Playwright Driver Successfully **************************************");
+        Log.bannerInfo("Created Playwright Driver Successfully");
     }
     @After
     public void tearDown(Scenario scenario) {
         if (scenario.isFailed()) {
-            Log.info("******************************** Scenario Failed, Capturing Screenshot *******************************");
+            Log.bannerInfo("Scenario Failed, Capturing Screenshot");
             byte[] screenshot = this.playwrightDriver.getPage().screenshot();
             scenario.attach(screenshot, "image/png", "Failed_Screenshot");
-            Log.info("******************************** Screenshot Captured Successfully *******************************");
+            Log.bannerInfo("Screenshot Captured & attached Successfully");
         }
         this.playwrightDriver.saveVideoForScenarioWithScenarioNames(scenario.getName());
         this.playwrightDriver.closeBrowser();
         this.playwrightDriver.quitPlaywright();
-        Log.info("******************************** Closed Browser,Playwright driver Successfully *******************************");
+        Log.bannerInfo("Closed Browser & Playwright driver Successfully");
     }
 }
